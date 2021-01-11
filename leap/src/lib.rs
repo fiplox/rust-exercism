fa52860f54941848e201268044ed07294fe65fb8
Volodymyr Patuta <6977238-fiplox@users.noreply.gitlab.com>
2021-01-11 15:21:13 +0100

    formating

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn is_leap_year(year: u64) -> bool {
     let mut res = false;
-    if (year%4)==0 && (year%100) != 0 || (year%400)==0 {
+    if (year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0 {
         res = true;
     }
     res
